52e56c83416dca6ba3024654b2dbef0ee182e5f2
Szymon Oleszek <szymqwerty@gmail.com>
2018-05-25 15:07:38 +0200

    WIP

--- test.brain.js
+++ test.brain.js
@@ -7,3 +7,97 @@
 // - can make sound with log "woof! woof!"
 // - randomly walking by 1 unit on area X:[-10,10], Y:[-10,10], (interval 1sec)
 // - create 5 different dogs
+
+
+
+class Position {
+    constructor() {
+        this.X = 0
+        this.Y = 0
+        this.color = this.choseColor()
+        setInterval(() => this.move(), 1000)
+    }
+    choseColor() {
+        const colorOption = Math.round(Math.random() * 5)
+        if (colorOption === 0) return 'blue'
+        else if (colorOption === 1) return 'green'
+        else if (colorOption === 2) return 'white'
+        else if (colorOption === 3) return 'red'
+        else return 'black'
+    }
+
+    choseMinusPlus() {
+        if (Math.random() > 0.5) return true
+        else return false
+    }
+    move() {
+
+
+
+
+        //let way = Math.round(Math.random() * 3)
+        if (this.choseMinusPlus()) {
+            this.X = this.X + 1
+            if (this.X === 10) this.X = this.X - 1
+            this.Y = this.Y + 1
+            if (this.Y === 10) this.Y = this.Y - 1
+        }
+
+        else if (this.choseMinusPlus()) {
+            this.X = this.X - 1
+            if (this.X === -10) this.X = this.X - 1
+            this.Y = this.Y - 1
+            if (this.Y === -10) this.Y = this.Y - 1
+        }
+
+    }
+
+
+}
+
+class Dog extends Position {
+    constructor(randomUserName) {
+
+        super()
+        this.picture = null;
+        setInterval(
+            () => this.activePosition(), 1000
+        )
+
+        fetch('https://randomuser.me/api/?results=1')
+            .then((response) => {
+                return response.json()
+            })
+            .then((j) => {
+                console.log(j.results)
+                this.picture = j.results[0].picture.large
+            }
+
+            )
+
+
+        this.name = randomUserName
+    }
+
+    activePosition() {
+
+        console.log(`DOG ${this.name} is on the position X: ${this.X} Y: ${this.Y}\n\n `)
+
+    }
+
+    makeSound() {
+        return "Woof woof";
+    }
+}
+
+
+const dog1 = new Dog('Reksio')
+const dog2 = new Dog('Rico')
+const dog3 = new Dog('Fox')
+const dog4 = new Dog('Brutus')
+const dog5 = new Dog('Baca')
+
+const zmienna = null
+
+
+
--- test.common.js
+++ test.common.js
@@ -1,24 +1,45 @@
 // min 2-3 tests for every function 
 
-const _unique = (_arrayOfNumbers)=>{
-    // return only unique numbers in new array
+const _unique = (list) => {
+
+    list = list.filter((x, i, a) => a.indexOf(x) == i) // index of znajduje pierwszy index
+
+    return list
 }
 
-const _isOdd = (value)=>{
+const _isOdd = (value) => {
     // if value is odd then true
+    return !(value % 2)
 }
 
-const _isEmail = (str)=>{
+const _isEmail = (str) => {
     // *if str is email-like then true 
     // find regexp on stackoverflow :)
 }
 
-const _mapForArray = (_array, callback)=>{
+const _mapForArray = (_array, callback) => {
     // use for/while loop
     // same functionalty as map
+    for (let i = 0; i < _array.length; i++) {
+        callback()
+    }
 }
 
-const _mapForObject = (_object, callback)=>{
+const _mapForObject = (_object, callback) => {
     // *use for/while loop
     // same functionalty as map but for object values
+    const arr = Object.values(_object)
+    for (let i = 0; i < arr.length; i++) {
+        callback()
+    }
 }
+
+
+
+console.log('1', _isOdd(2), "2", _isOdd(4), '3', _isOdd(23))
+
+_mapForArray([1, 2, 3, 4], () => console.log('dziala'))
+
+_mapForObject({ name: 'szymon', age: 23, height: 178 }, () => console.log('works'))
+
+console.log(_unique([1, 1, 1, 2, 3, 1, 2, 3, 4, 4, 5]))
